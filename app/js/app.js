b7cb1b7ee9a94f68eac575b0f8cd974a28df947f
yutaka-sano <yutaka.sano@dena.com>
2015-06-16 14:00:21 +0900

    implemented audio reactive

--- app/js/app.js
+++ app/js/app.js
@@ -38,6 +38,13 @@ var AudioManager = (function () {
 })();
 var App = (function () {
     function App() {
+        var _this = this;
+        this.isWireFrame = false;
+        this.onWindowResize = function () {
+            this.camera.aspect = window.innerWidth / window.innerHeight;
+            this.camera.updateProjectionMatrix();
+            this.renderer.setSize(window.innerWidth, window.innerHeight);
+        };
         console.log("app start");
         var CLIENT_ID = 'd4668edbb5d755565ca2079b70b35576';
         var TRACK_URL = 'https://soundcloud.com/dj_potato_salad/d-j-potato-salad-presents-y-a';
@@ -64,9 +71,88 @@ var App = (function () {
             var bytes = new Uint8Array(analyser.frequencyBinCount);
             var drawId = setInterval(function () {
                 analyser.getByteFrequencyData(bytes);
+                var d = bytes[8];
+                _this.cube.scale.set(d / 100, d / 100, d / 100);
+                _this.renderer.render(_this.scene, _this.camera);
             }, 1000 / 60);
+            _this.initThreeJS();
         });
     }
+    App.prototype.initThreeJS = function () {
+        var _this = this;
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+        this.camera.position.set(0, 70, 70);
+        this.scene = new THREE.Scene();
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: false,
+            clearColor: 0x000000,
+            clearAlpha: 0,
+            alpha: true,
+            preserveDrawingBuffer: true
+        });
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setClearColor(0xffffff);
+        this.renderer.shadowMapEnabled = true;
+        this.container = document.getElementById('container');
+        this.container.appendChild(this.renderer.domElement);
+        this.onWindowResize();
+        window.addEventListener("resize", this.onWindowResize, false);
+        var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+        directionalLight.position.set(0, 100, 30);
+        directionalLight.castShadow = true;
+        this.scene.add(directionalLight);
+        var geometry = new THREE.CubeGeometry(40, 40, 40);
+        this.material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+        this.cube = new THREE.Mesh(geometry, this.material);
+        this.cube.position.set(0, 0, 0);
+        this.cube.castShadow = true;
+        this.scene.add(this.cube);
+        var axis = new THREE.AxisHelper(1000);
+        axis.position.set(0, 0, 0);
+        this.scene.add(axis);
+        this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+        this.container.addEventListener("mousemove", (function (e) {
+            var mouseX, mouseY;
+            mouseX = e.clientX - 600 / 2;
+            mouseY = e.clientY - 400 / 2;
+        }), false);
+        this.canvas = document.getElementById('visualizer');
+        this.canvasContext = this.canvas.getContext('2d');
+        var gui = new dat.GUI();
+        var wireframeControl = gui.add(this, 'isWireFrame');
+        wireframeControl.onChange(function (value) {
+            _this.material.wireframe = value;
+        });
+        window.addEventListener("keyup", function (e) {
+            var imgData, imgNode;
+            if (e.which !== 80)
+                return;
+            try {
+                imgData = _this.renderer.domElement.toDataURL();
+                console.log(imgData);
+            }
+            catch (e) {
+                console.log(e);
+                console.log("Browser does not support taking screenshot of 3d context");
+                return;
+            }
+        });
+        this.animate();
+    };
+    App.prototype.update = function () {
+    };
+    App.prototype.draw = function (spectrums) {
+    };
+    App.prototype.render = function () {
+        this.renderer.render(this.scene, this.camera);
+    };
+    App.prototype.animate = function () {
+        var _this = this;
+        this.update();
+        this.draw(this.spectrums);
+        requestAnimationFrame(function (e) { return _this.animate(); });
+        this.render();
+    };
     return App;
 })();
 window.addEventListener("load", function (e) {
--- src/typescript/app.ts
+++ src/typescript/app.ts
@@ -15,6 +15,22 @@ declare var SC: {
 };
 
 class App {
+
+	private scene:THREE.Scene;
+	private camera:THREE.PerspectiveCamera;
+	private renderer;
+	private container;
+	private controls;
+	private cube;
+	private material;
+	private isWireFrame:Boolean =false;
+	private animationId;
+	private audioManager:AudioManager;
+	private canvas;
+	private canvasContext;
+	private spectrums;
+
+
 	constructor() {
 		console.log("app start");
 		var CLIENT_ID = 'd4668edbb5d755565ca2079b70b35576';
@@ -53,13 +69,142 @@ class App {
 			var drawId = setInterval(()=>{
 				analyser.getByteFrequencyData(bytes);
 				//console.log(bytes);
+				var d = bytes[8];
+				//this.cube.position.y = d
+				this.cube.scale.set(d / 100, d / 100, d / 100);
+				this.renderer.render(this.scene, this.camera);
+
 			},1000/60)
 
+			this.initThreeJS();
+
 		});
+	}
+
+	private initThreeJS(){
+		//1.カメラ追加
+		this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+		this.camera.position.set(0, 70, 70);
+
+		//2.シーン追加
+		this.scene = new THREE.Scene();
+
+		//3.レンダラー追加
+		this.renderer = new THREE.WebGLRenderer({
+			antialias: false,
+			clearColor: 0x000000,
+			clearAlpha: 0,
+			alpha: true,
+			preserveDrawingBuffer: true
+		});
+		this.renderer.setPixelRatio(window.devicePixelRatio);
+		this.renderer.setClearColor(0xffffff);
+		this.renderer.shadowMapEnabled = true;
+
+		//4.表示コンテナ指定
+		this.container = document.getElementById('container');
+		this.container.appendChild(this.renderer.domElement);
+		//リサイズ処理
+		this.onWindowResize();
+		window.addEventListener("resize", this.onWindowResize, false);
+
+		//5 オブジェクト追加
+		//光源追加
+		var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+		directionalLight.position.set(0, 100, 30);
+		directionalLight.castShadow = true;
+		this.scene.add(directionalLight);
+		//cube追加
+		var geometry = new THREE.CubeGeometry(40, 40, 40);
+		this.material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+		this.cube = new THREE.Mesh(geometry, this.material);
+		this.cube.position.set(0, 0, 0);
+		this.cube.castShadow = true;
+		this.scene.add(this.cube);
+		//座標軸追加
+		var axis = new THREE.AxisHelper(1000);
+		axis.position.set(0, 0, 0);
+		this.scene.add(axis);
+
+		//マウス制御機能追加
+		this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+		this.container.addEventListener("mousemove", ((e) => {
+			var mouseX, mouseY;
+			mouseX = e.clientX - 600 / 2;
+			mouseY = e.clientY - 400 / 2;
+		}), false);
+
+		this.canvas = document.getElementById('visualizer');
+		this.canvasContext = this.canvas.getContext('2d');
+		//this.canvas.setAttribute('width', this.audioManager.getAnalyser().frequencyBinCount * 10);
+
+		var gui = new dat.GUI();
+		var wireframeControl = gui.add(this, 'isWireFrame');
+		wireframeControl.onChange( (value)=> {
+			this.material.wireframe = value
+		});
+
+		/*** ADDING SCREEN SHOT ABILITY ***/
+		window.addEventListener("keyup", (e)=>{
+			var imgData, imgNode;
+			//Listen to 'P' key
+			if(e.which !== 80) return;
+			try {
+				imgData = this.renderer.domElement.toDataURL();
+				console.log(imgData);
+			}
+			catch(e) {
+				console.log(e)
+				console.log("Browser does not support taking screenshot of 3d context");
+				return;
+			}
+		});
+
+		this.animate();
+	}
+
+	private onWindowResize = function () {
+		this.camera.aspect = window.innerWidth / window.innerHeight;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+	}
+
+	private update() {
+		//this.spectrums = this.audioManager.getSpectrum();
+		//this.draw(spectrums);
+	}
+
+	private draw(spectrums) {
+		//描画
+		//spectrums 0 - 200の範囲
+
+		//this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		//for (var i = 0, len = spectrums.length; i < len; i++) {
+		//	this.canvasContext.fillRect(i * 10, 0, 5, spectrums[i]);
+		//}
+	}
+
+	private render() {
+		//var d = 50;
+		//this.cube.position.y = d
+		//this.cube.scale.set(d / 100, d / 100, d / 100);
+		this.renderer.render(this.scene, this.camera);
+	}
+
+	public animate() {
+		this.update();
+		this.draw(this.spectrums);
+
+		requestAnimationFrame((e)=>
+				this.animate()
+		);
+		this.render();
 	}
 }
 
 window.addEventListener("load", (e) => {
 	console.log('loaded');
 	var app = new App();
+
+
 });
